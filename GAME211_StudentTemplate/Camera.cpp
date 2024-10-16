c4066cb26ea7e9bb0c985a3df0940d6b7c317588
Ralph Robes <98129737+RalphRobes0324@users.noreply.github.com>
2024-10-16 08:45:36 -0400

    Created the Base of Camera Script

--- GAME211_StudentTemplate/Camera.cpp
+++ GAME211_StudentTemplate/Camera.cpp
@@ -1,5 +1,14 @@
 #include "Camera.h"
 
-Camera::Camera()
+Camera::Camera(int _screenWidth, int _screenHeight) :
+	screenWidth(_screenWidth), screenHeight(_screenHeight)
 {
+	gameCamera = { 0, 0, screenWidth, screenHeight };
 }
+
+void Camera::Update(float _targetX, float _targetY, GameManager* _game)
+{
+
+}
+
+
--- GAME211_StudentTemplate/Camera.h
+++ GAME211_StudentTemplate/Camera.h
@@ -2,10 +2,18 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <SDL.h>
+#include "GameManager.h"
+
 class Camera {
 public:
-	Camera();
+	Camera(int _screenWidth, int _screenHeight);
+	void Update(float _targetX, float _targetY, GameManager* _game);
+
 private:
+	SDL_Rect gameCamera;
+	int screenWidth;
+	int screenHeight;
 };
 
 
